8f9b000df3429f96fcb8910e62ae0ac7033eefb5
jjuanx <jpozogarcia2017@gmail.com>
2025-07-12 22:36:35 +0200

    Initial-Commit

---
+++ DeliverUS-Backend/src/routes/PerformancesRoutes.js
@@ -0,0 +1,11 @@
+// This is a new file for solution!
+import { hasRole, isLoggedIn } from '../middlewares/AuthMiddleware.js'
+import * as PerformanceMiddleware from '../middlewares/PerformanceMiddleware.js'
+import { handleValidation } from '../middlewares/ValidationHandlingMiddleware.js'
+
+const loadFileRoutes = (app) => {
+  // TO-DO
+  // Introduce the needed routing
+}
+
+export default loadFileRoutes
